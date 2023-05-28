ba739c247d4501a4dbb5b261db21311f35ab0772
Jean-Baptiste Bouvier <124528248+Jean-BaptisteBouvier@users.noreply.github.com>
2023-05-28 15:29:13 +0200

    Add files via upload

--- JavaScript/chart.js
+++ JavaScript/chart.js
@@ -6,13 +6,15 @@ using data scraped from Google Scholar using the Python script
 */
 
 /* Data scraped from Google Scholar with Python script*/
-var total_citations = 60;
+var total_citations = 61;
 var h_index = 4;
 var i10_index = 2;
-var last_update = "May 8th 2023";
+var last_update = "May 23 2023";
 var years_cited = [2018, 2019, 2020, 2021, 2022, 2023];
-var citations_by_year = [2, 3, 4, 9, 24, 12];
-	
+var citations_per_year = [2, 3, 4, 9, 23, 20];
+var citations_per_paper = [15, 12, 8, 6, 4, 4, 4, 3, 1, 1, 1, 1, 1, 0, 0];
+var titles = ['Preliminary design of low-energy, low-thrust transfers to halo orbits using feedback control', 'Quantitative resilience of linear driftless systems', 'Resilient reachability for linear systems', 'Designing resilient linear systems', 'Quantitative Resilience of Linear Systems', 'Designing resilient linear driftless systems', 'Spacecraft trajectory tracking and parameter estimation around a splitting contact binary asteroid', 'The Maximax Minimax Quotient Theorem', 'Resilience of linear systems to partial loss of control authority', 'Distributed Transient Safety Verification via Robust Control Invariant Sets: A Microgrid Application', 'Resilience of orbital inspections to partial loss of control authority of the chaser satellite', 'Quantitative Resilience of Generalized Integrators', 'Orbit control for a spacecraft around a splitting contact binary asteroid', 'Resilient Trajectory Tracking to Partial Loss of Control Authority over Actuators with Actuation Delay', 'Assured System-Level Resilience for Guaranteed Disaster Response'];
+var links_citations = ['https://scholar.google.com/scholar?oi=bibs&hl=en&cites=15867869489605474014', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=1674438979349476753', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=12994462789597643066', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=16589064241543812386', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=13481627010641748787', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=585800018701299105', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=15189642400668903976', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=12204788610249439923', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=283779437568804643', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=6744962304205001077', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=4814518497430150789', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=13678218939683511340', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=14647554166549373763', '', ''];
 	
   
 /* Getting the table to write citations and indices */
@@ -45,7 +47,7 @@ const hist = new Chart(ctx, {
   data: {
     labels: years_cited,
     datasets: [{
-      data: citations_by_year,
+      data: citations_per_year,
       borderWidth: 0,
 	  backgroundColor: 'rgba(50, 50, 50, 0.8)',
 	  barThickness: 20,
@@ -64,3 +66,11 @@ const hist = new Chart(ctx, {
 const col_width = 40
 document.getElementById('histogram').style.width = (hist.data.labels.length + 1)*col_width + "px"
 
+
+
+/* Adding the number of citations to each paper*/
+var nb_papers = citations_per_paper.length;
+for (let i = 0; i < nb_papers; i++){
+	document.getElementById(titles[i]).innerHTML = citations_per_paper[i];
+	document.getElementById(titles[i]).href = links_citations[i];
+}
