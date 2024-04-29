c6753610cecf41bbad3550b745fe9992a734d3a0
Jean-Baptiste Bouvier <124528248+Jean-BaptisteBouvier@users.noreply.github.com>
2024-04-29 12:12:18 -0700

    Update chart.js

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -6,13 +6,13 @@ using data scraped from Google Scholar using the Python script
 */
 
 /* Data scraped from Google Scholar with Python script*/
-var total_citations = 89;
+var total_citations = 94;
 var h_index = 5;
 var i10_index = 3;
-var last_update = "December 08 2023";
-var years_cited = [2018, 2019, 2020, 2021, 2022, 2023];
-var citations_per_year = [2, 3, 3, 6, 22, 53];
-var citations_per_paper = [21, 15, 10, 9, 7, 4, 4, 4, 4, 3, 2, 2, 2, 1, 1, 0, 0, 0];
+var last_update = "April 30 2024";
+var years_cited = [2018, 2019, 2020, 2021, 2022, 2023, 2024];
+var citations_per_year = [2, 3, 3, 6, 22, 53, 5];
+var citations_per_paper = [25, 16, 10, 9, 7, 4, 4, 4, 4, 3, 2, 2, 2, 1, 1, 0, 0, 0];
 var titles = ['Preliminary design of low-energy, low-thrust transfers to halo orbits using feedback control', 'Quantitative resilience of linear driftless systems', 'Resilient reachability for linear systems', 'Designing resilient linear systems', 'Quantitative Resilience of Linear Systems', 'Distributed Transient Safety Verification via Robust Control Invariant Sets: A Microgrid Application', 'The Maximax Minimax Quotient Theorem', 'Designing resilient linear driftless systems', 'Spacecraft trajectory tracking and parameter estimation around a splitting contact binary asteroid', 'Resilience of linear systems to partial loss of control authority', 'Quantitative resilience of generalized integrators', 'Assured System-Level Resilience for Guaranteed Disaster Response', 'Resilience of orbital inspections to partial loss of control authority of the chaser satellite', 'Resilient Trajectory Tracking to Partial Loss of Control Authority over Actuators with Actuation Delay', 'Orbit control for a spacecraft around a splitting contact binary asteroid', 'Losing Control of your Network? Try Resilience Theory', 'Guaranteed resilience of autonomous systems to partial loss of control authority over actuators', 'Strategies for space rendezvous on Lunar Distant Retrograde Orbits'];
 var links_citations = ['https://scholar.google.com/scholar?oi=bibs&hl=en&cites=15867869489605474014', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=1674438979349476753', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=12994462789597643066', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=16589064241543812386', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=13481627010641748787', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=6744962304205001077', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=12204788610249439923', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=585800018701299105', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=15189642400668903976', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=5139918215096357178', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=8089023666874970497', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=10196610562663343129', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=18315162202588692504', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=8047868568380548501', 'https://scholar.google.com/scholar?oi=bibs&hl=en&cites=14647554166549373763', '', '', ''];
 	
